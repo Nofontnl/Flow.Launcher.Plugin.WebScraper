4903d864b6936a58bd7f4faf09d23a94584040ac
Nofontnl <56501567+Nofontnl@users.noreply.github.com>
2026-03-16 00:06:49 +0100

    Miscellaneous required plugin files

---
+++ release.ps1
@@ -0,0 +1,2 @@
+dotnet publish Flow.Launcher.Plugin.WebScraper -c Release -r win-x64 --no-self-contained
+Compress-Archive -LiteralPath Flow.Launcher.Plugin.WebScraper/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.WebScraper/bin/WebScraper.zip -Force
